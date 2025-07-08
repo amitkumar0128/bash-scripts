774beb6ddb16e6238fa0ab7e2b80a6f299e3dbd2
amitkumar_0128 <jangidamitkumar49@gmail.com>
2025-07-08 20:49:28 +0530

    refactor: enhance hello-user-script.sh usability

--- hello-user-script.sh
+++ hello-user-script.sh
@@ -2,8 +2,8 @@
 
 read -p "Enter your name: " name
 if [ -z "$name" ]; then
-    echo "No name provided. Exiting."
+    echo "No name provided. Exiting." >&2
     exit 1
 fi
 
-echo "Hello, $name! This is hello-user-script.sh"
+echo "Hello, $name! This is $(basename "$0")"
