c7f6ef447c0213b21026a966cf5c66af67572d51
amitkumar_0128 <jangidamitkumar49@gmail.com>
2025-07-08 20:22:11 +0530

    feat: auto-system-update.sh

---
+++ auto-system-update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if sudo apt-get update; then
+    echo ""
+    echo -e "\e[1;32mSystem upgraded succesfully.\e[0m"
+    echo ""
+else
+    echo -e "\e[1;31mSystem update failed\e[0m" >&2
+    exit 1
+fi
+
+if sudo apt-get upgrade -y; then
+    echo ""
+    echo -e "\e[1;32mSystem upgraded succesfully.\e[0m"
+    echo ""
+else
+    echo -e "\e[1;31mSystem update failed\e[0m" >&2
+    exit 1
+fi
